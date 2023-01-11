1ef7d2cbbc7dee9cf32b52d25c11a790f0e226fb
Umarocks <54461790+Umarocks@users.noreply.github.com>
2023-01-11 19:41:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const form = document.querySelector('#dropDown');
+const result = document.querySelector('#tableResult');
+var update;
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    if(update){
+        clearTimeout();
+    }
+    const ctype = form.elements.coinType.value;
+    fetchPrice(ctype);
+})
+
+
+const fetchPrice= async(ctype) =>{
+    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=USD`);
+    console.log(r.data.coin.price);
+     const price = r.data.coin.price;
+     const volume  = r.data.coin.volume;
+     const change = r.data.coin.priceChange1d;
+     const base = r.data.coin.name;
+     const target = 'USD';
+     result.innerHTML =`<tr style="color:white; font-weight:700">
+     <td>
+         Property
+     </td>
+     <td>Value</td>
+ </tr>
+ <tr>
+     <td>
+         ${base}
+     </td>
+     <td>${price} ${target}</td>
+ </tr>
+ <tr>
+     <td>
+         Volume
+     </td>
+     <td>${volume}</td>
+ </tr>
+ <tr>
+     <td>
+         Change
+     </td>
+     <td>${change}</td>
+ </tr>`
+
+    update = setTimeout(()=>fetchPrice(ctype),10000);
+
+}
